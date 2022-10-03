94ab83f99a4c9908328a562cc32f02d8d10b6af9
Anton <antonrozdobudko@gmail.com>
2022-10-03 21:34:21 +1000

    Scroll inside the main blocks has been added for Portfolio.

--- src/components/App/PortfolioMain/PortfolioMain.tsx
+++ src/components/App/PortfolioMain/PortfolioMain.tsx
@@ -2,13 +2,25 @@ import styles from './PortfolioMain.module.scss'
 import {CloseButton} from '../MainPage/Common/CloseButton/CloseButton';
 import {Header} from '../MainPage/Common/Header/Header';
 import {faSuitcase} from '@fortawesome/free-solid-svg-icons';
+import {useCustomSelector} from '../../../bll/main/store';
+import {PageType} from '../../../bll/main/appReducer';
 
 export const PortfolioMain = () => {
+    const page = useCustomSelector<PageType>(state => state.app.page)
 
     return (
         <div className={styles.container}>
             <CloseButton/>
-            <Header contentFirst='My' contentSecond='Portfolio' icon={faSuitcase}/>
+            <Header contentFirst="My" contentSecond="Portfolio" icon={faSuitcase}/>
+            <div className={page === 'portfolio' ? styles.content : styles.contentNone}>
+                <div className={styles.big}></div>
+                <div className={styles.big}></div>
+                <div className={styles.big}></div>
+                <div className={styles.big}></div>
+                <div className={styles.big}></div>
+                <div className={styles.big}></div>
+                <div className={styles.big}></div>
+            </div>
         </div>
     )
 }
